97ec157a45c8453c5e75c9c0bea5611a4eddffa0
nazaninhesari <nazanin.hesari@yahoo.com>
2023-10-24 15:07:28 +0330

    initial

---
+++ app/layout.jsx
@@ -0,0 +1,11 @@
+import "./../styles/globals.css";
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="fa-IR">
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
